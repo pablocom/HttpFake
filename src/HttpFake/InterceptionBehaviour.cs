b0c4eb8eb91e2c982be72d08d9e7c95127d90714
Pablo Company <pcompany@ecovadis.com>
2023-07-07 10:51:02 +0200

    refactor: Moves query params specification tests to a separate file

--- src/HttpFake/InterceptionBehaviour.cs
+++ src/HttpFake/InterceptionBehaviour.cs
@@ -5,10 +5,3 @@ public enum InterceptionBehaviour
     Strict = 0, 
     Lax = 1
 }
-
-public class NoReceivedRequestReceivedRequestMatchingException : Exception
-{
-    public NoReceivedRequestReceivedRequestMatchingException(string message) : base(message)
-    {
-    }
-}
--- tests/HttpFake.UnitTests/WhenInterceptingRequestByQueryParams.cs
+++ tests/HttpFake.UnitTests/WhenInterceptingRequestByQueryParams.cs
@@ -1,46 +1,7 @@
 ﻿using System.Net;
 using FluentAssertions;
-using HttpFake.Specifications;
 
 namespace HttpFake.UnitTests;
-
-public sealed class WhenInterceptingRequestByHttpMethod
-{
-    private static readonly Uri AnyBaseUri = new("https://pablocompany.com");
-    private static readonly HttpResponseMessage ConfiguredResponse = new(HttpStatusCode.Found);
-
-    [Fact]
-    public async Task MatchesRequestWithConfiguredResponseOnlyByHttpMethod()
-    {
-        var interceptor = new ConfiguredHttpRequestsInterceptor();
-        interceptor.Register(new ConfiguredResponseBuilder()
-            .WithSpecification(new HttpMethodSpecification(HttpMethod.Get))
-            .RespondWith(ConfiguredResponse)
-            .Build());
-
-        var request = new HttpRequestMessage(HttpMethod.Get, new Uri(AnyBaseUri, "/api/endpoint"));
-        var interceptionResult = await interceptor.Intercept(request);
-
-        interceptionResult.HasMatchedWithConfiguredResponse.Should().BeTrue();
-        interceptionResult.ConfiguredRequest!.Response.Should().Be(ConfiguredResponse);
-    }
-
-    [Fact]
-    public async Task DoesNotInterceptRequestIfMethodIsNotEqual()
-    {
-        var interceptor = new ConfiguredHttpRequestsInterceptor();
-        interceptor.Register(new ConfiguredResponseBuilder()
-            .WithSpecification(new HttpMethodSpecification(HttpMethod.Get))
-            .RespondWith(ConfiguredResponse)
-            .Build());
-
-        var request = new HttpRequestMessage(HttpMethod.Patch, new Uri(AnyBaseUri, "/api/endpoint/path?First=firstValue&Second=secondValue"));
-        var interceptionResult = await interceptor.Intercept(request);
-
-        interceptionResult.HasMatchedWithConfiguredResponse.Should().BeFalse();
-        interceptionResult.ConfiguredRequest.Should().BeNull();
-    }
-}
 
 public sealed class WhenInterceptingRequestByQueryParams
 {
